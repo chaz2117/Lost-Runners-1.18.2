410f8311ee7c107eb2df9519016a863971397d92
chaz2117 <charleyabbott4@gmail.com>
2022-06-17 15:14:05 +0100

    Initial Commit

---
+++ src/main/java/net/chaz2117/lostrunners/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.chaz2117.lostrunners.mixin;
+
+import net.chaz2117.lostrunners.LostRunners;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		LostRunners.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
